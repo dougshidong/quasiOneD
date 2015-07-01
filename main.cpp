6488262d821609026bbd07c8a67403f717c00ab8
Doug Shi-Dong <doug.shidong@gmail.com>
2015-07-01 02:42:19 -0400

    added main

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "quasiOneD.h"
+#include <iostream>
+
+int main()
+{
+	std::cout<<quasiOneD();
+}
---
+++ quasiOneD.h
@@ -0,0 +1,9 @@
+#ifndef QUASIONED_H
+#define QUASIONED_H
+
+int main();
+
+double isenP(double pt, double M);
+double isenT(double Tt, double M);
+
+#endif
