0d259f8340b75112404a58228c89687deecfe983
Doug Shi-Dong <doug.shidong@gmail.com>
2015-07-13 22:53:28 -0400

    stegerwarming implemented

--- quasiOneD.cpp
+++ quasiOneD.cpp
@@ -4,7 +4,7 @@
 
 // Discretization
 
-int nx=50;
+const int nx=50;
 double a=0, b=1;
 double dx=(b-a)/(nx-1);
 
@@ -27,6 +27,7 @@ double pexit=0.8*ptin;
 
 // Convergence Settings
 double CFL=0.5;
+double eps=0.3;
 double normR=1;
 double conv=1e-6;
 int iterations=0;
@@ -36,7 +37,7 @@ int maxIt=20;
 
 double isenP(double pt, double M);
 double isenT(double Tt, double M);
-void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double c[], double rho[])
+void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double c[], double rho[]);
 
 int quasiOneD()
 {
@@ -121,7 +122,20 @@ int quasiOneD()
 		for(int i=0;i<nx;i++)
 			dt[i]=(CFL*dx)/maxUC;
 
-		Flux_StegerWarming(Flux,F,u,c,W);
+		Flux_StegerWarming(Flux,W,u,c,rho);
+
+		// Euler Explicit
+		//
+		for(int k=0;k<3;k++)
+		{
+			for(int i=0;i<nx-1;i++)
+				Resi[k][i+1]=Flux[k][i+1]*S[i+1]-Flux[k][i]*S[i]-Q[k][i+1];
+			Resi[k][0]=0;
+			Resi[k][nx-1]=0;
+		}
+		for(int k=0;k<3;k++)
+			for(int i=0;i<nx-1;i++)
+				W[k][i]=W[k][i]-(dt[i]/V[i-1])*Resi[k][i];
 	}
 
 	return 0;
@@ -137,26 +151,46 @@ double isenT(double Tt, double M)
 	return Tt*pow((1+(gam-1)/2*pow(M,2)),-1);
 }
 
+
+void matrixMult(double A[3][3], double B[3][3], double result[3][3])
+{
+    double temp[3][3];
+    for(int row=0;row<3;row++)
+        for(int col=0;col<3;col++)
+            temp[row][col]=0;
+
+    for(int row=0;row<3;row++)
+        for(int col=0;col<3;col++)
+        {
+            for(int k=0;k<3;k++)
+                temp[row][col]+=A[row][k]*B[k][col];
+        }
+    for(int row=0;row<3;row++)
+        for(int col=0;col<3;col++)
+            result[row][col]=temp[row][col];
+}
+
 void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double c[], double rho[])
 {
 	double S[nx-1][3][3],Sinv[nx-1][3][3],C[nx-1][3][3],Cinv[nx-1][3][3];
-	double lambaP[nx-1][3][3],lambdaN[nx-1][3][3];
+	double lambdaP[nx-1][3][3],lambdaN[nx-1][3][3];
 
-	double Ap[3][3],An[3][3];
-	double beta=gamma-1;
+	double Ap[nx-1][3][3],An[nx-1][3][3];
+	double tempP[3][3], tempN[3][3], prefixM[3][3],suffixM[3][3];
+	double beta=gam-1;
 	double lambdaa[nx-1][3];
 
 
 	memset(Flux,0,sizeof(Flux[0][0])*3*nx-1);
 	memset(S,0,sizeof(S[0][0][0])*2*3*3);
-	memset(Sinv,0,sizeof(Sinv[0][0][0])2*3*3);
+	memset(Sinv,0,sizeof(Sinv[0][0][0])*2*3*3);
 	memset(C,0,sizeof(C[0][0][0])*2*3*3);
-	memset(Cinv,0,sizeof(Cinv[0][0][0])2*3*3);
+	memset(Cinv,0,sizeof(Cinv[0][0][0])*2*3*3);
 	memset(lambdaP,0,sizeof(lambdaP[0][0][0])*2*3*3);
 	memset(lambdaN,0,sizeof(lambdaN[0][0][0])*2*3*3);
 
 
-	for(i=0;i<nx-1;i++)
+	for(int i=0;i<nx-1;i++)
 	{
 		S[i][0][0]=1;
 		S[i][1][0]=-u[i]/rho[i];
@@ -166,7 +200,7 @@ void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double 
 		S[i][2][2]=beta;
 		Sinv[i][0][0]=1;
 		Sinv[i][1][0]=u[i];
-		Sinv[i][2][0]=0.5*u[i]*u[i]
+		Sinv[i][2][0]=0.5*u[i]*u[i];
 		Sinv[i][1][1]=rho[i];
 		Sinv[i][2][1]=u[i]*rho[i];
 		Sinv[i][2][2]=1/beta;
@@ -183,18 +217,51 @@ void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double 
 		Cinv[i][1][2]=-1/(2*rho[i]*c[i]);
 		Cinv[i][2][1]=0.5;
 		Cinv[i][2][2]=0.5;
-		eigenvalue[i][0]=u[i];
-		eigenvalue[i][1]=u[i]+c[i];
-		eigenvalue[i][2]=u[i]-c[i];
+		lambdaa[i][0]=u[i];
+		lambdaa[i][1]=u[i]+c[i];
+		lambdaa[i][2]=u[i]-c[i];
 
 		for(int k=0;k<3;k++)
 			if(lambdaa[i][k]>0)
-				lambdaP[i][k][k]=lambdaa[k]*sqrt(pow(lambdaa[i][k],2)+pow(eps,2))/2;
+				lambdaP[i][k][k]=lambdaa[i][k]*
+					sqrt(pow(lambdaa[i][k],2)+pow(eps,2))/2;
 			else
-				lambdaN[i][k][k]=lambdaa[k]*sqrt(pow(lambdaa[i][k],2)+pow(eps,2))/2;
-
-
+				lambdaN[i][k][k]=lambdaa[i][k]*
+					sqrt(pow(lambdaa[i][k],2)+pow(eps,2))/2;
 	}
+
+	for(int i=0;i<nx-1;i++)
+	{
+		memset(Ap,0,sizeof(Ap[0][0])*3*3);
+		memset(An,0,sizeof(Ap[0][0])*3*3);
+		for(int row=0;row<3;row++)
+		for(int col=0;col<3;col++)
+			for(int k=0;i<3;k++)
+			{
+				prefixM[row][col]+=Sinv[i][row][k]*Cinv[i][k][col];
+				suffixM[row][col]+=C[i][row][k]*S[i][k][col];
+			}
+		for(int row=0;row<3;row++)
+		for(int col=0;col<3;col++)
+			for(int k=0;i<3;k++)
+			{
+				tempP[row][col]=prefixM[row][k]*lambdaP[i][k][col];
+				tempN[row][col]=prefixM[row][k]*lambdaN[i][k][col];
+			}
+		for(int row=0;row<3;row++)
+		for(int col=0;col<3;col++)
+			for(int k=0;i<3;k++)
+			{
+				Ap[i][row][col]=tempP[row][k]*suffixM[k][col];
+				An[i][row][col]=tempN[row][k]*suffixM[k][col];
+			}
+		for(int row=0;row<3;row++)
+		for(int col=0;col<3;col++)
+			Flux[row][i]=Flux[row][i]+Ap[i][row][col]*W[col][i]
+				+An[i][row][col]*W[col][i+1];
+	}
+
+
 }
 
 
--- quasiOneD.h
+++ quasiOneD.h
@@ -3,7 +3,7 @@
 
 int quasiOneD();
 
-double isenP(double pt, double M);
-double isenT(double Tt, double M);
-void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double c[], double rho[]);
+//double isenP(double pt, double M);
+//double isenT(double Tt, double M);
+//void Flux_StegerWarming(double Flux[][nx-1], double W[][nx], double u[], double c[], double rho[]);
 #endif
