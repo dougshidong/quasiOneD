363e9db6d072a138f128d3e93a80601347636a42
dougshidong <doug.shidong@gmail.com>
2016-01-05 18:16:29 -0500

    retabbed quasioned.cpp

--- quasiOneD.cpp
+++ quasiOneD.cpp
@@ -19,352 +19,352 @@ double TotalPressureLoss(std::vector <double> W);
 void ioTargetPressure(int io, std::vector <double> &p);
 
 double inverseFitness(std::vector <double> pcurrent, std::vector <double> ptarget,
-		std::vector <double> dx);
+        std::vector <double> dx);
 
 double quasiOneD(std::vector <double> x, 
-		std::vector <double> dx, 
-		std::vector <double> S,
-		std::vector <double> designVar,
-		std::vector <double> &W)
+        std::vector <double> dx, 
+        std::vector <double> S,
+        std::vector <double> designVar,
+        std::vector <double> &W)
 {
-	std::vector <double> rho(nx), u(nx), e(nx);
-	std::vector <double> T(nx), p(nx), c(nx), Mach(nx);
-	std::vector <double> F(3 * nx, 0), Q(3 * nx, 0), Resi(3 * nx, 0);
-	std::vector <double> Flux(3 * (nx + 1), 0);
-//	std::vector <std::vector <double> > W(3, std::vector <double> (nx, 0)),
-//					    F(3, std::vector <double> (nx, 0)),
-//					    Q(3, std::vector <double> (nx, 0));
+    std::vector <double> rho(nx), u(nx), e(nx);
+    std::vector <double> T(nx), p(nx), c(nx), Mach(nx);
+    std::vector <double> F(3 * nx, 0), Q(3 * nx, 0), Resi(3 * nx, 0);
+    std::vector <double> Flux(3 * (nx + 1), 0);
+//  std::vector <std::vector <double> > W(3, std::vector <double> (nx, 0)),
+//                      F(3, std::vector <double> (nx, 0)),
+//                      Q(3, std::vector <double> (nx, 0));
 
-//	std::vector <std::vector <double> > Flux(3, std::vector <double> (nx + 1));
+//  std::vector <std::vector <double> > Flux(3, std::vector <double> (nx + 1));
 
-//	std::vector <std::vector <double> > Resi(3, std::vector <double> (nx, 0)),
+//  std::vector <std::vector <double> > Resi(3, std::vector <double> (nx, 0)),
 
-	std::vector <double> dt(nx), V(nx);
+    std::vector <double> dt(nx), V(nx);
 
-	std::vector <int> itV(maxIt/printIt);
-	std::vector <double> normV(maxIt/printIt);
+    std::vector <int> itV(maxIt/printIt);
+    std::vector <double> normV(maxIt/printIt);
 
-	double dpdu, avgu, avgc, dtdx, dpdx, dudx;
-	std::vector <double> eigenvalues(3);
-	std::vector <double> charRel(3);
-	double dp, drho, du;
-	double MachBound;
+    double dpdu, avgu, avgc, dtdx, dpdx, dudx;
+    std::vector <double> eigenvalues(3);
+    std::vector <double> charRel(3);
+    double dp, drho, du;
+    double MachBound;
 
-	int iterlength;
-	double normR = 1.0;
-	int iterations = 0;
-
-
-	V = calcVolume(S, dx);
+    int iterlength;
+    double normR = 1.0;
+    int iterations = 0;
 
 
-	// Inlet flow properties
-	Mach[0]=Min;
-	T[0] = isenT(Ttin, Mach[0]);
-	p[0] = isenP(ptin, Mach[0]);
-	rho[0] = p[0] / (R * T[0]);
-	c[0] = sqrt(gam * p[0] / rho[0]);
-	u[0] = Mach[0] * c[0];
-	e[0] = rho[0] * (Cv * T[0] + 0.5 * pow(u[0], 2));
+    V = calcVolume(S, dx);
 
-	// Flow Properties Initialization
-	for(int i = 1; i < nx; i++)
-	{
-		Mach[i] = Min;
-		Mach[i] = Min;
-		p[i] = pexit;
-		T[i] = T[0];
-		rho[i] = p[i] / (R * T[i]);
-		c[i] = sqrt(gam * p[i] / rho[i]);
-		u[i] = c[i] * Mach[i];
-		e[i] = rho[i] * (Cv * T[i] + 0.5 * pow(u[i], 2));
-	}
 
-	// State and Flux Vectors Initialization
-	for(int i = 0; i < nx; i++)
-	{
-		W[0 * nx + i] = rho[i];
-		W[1 * nx + i] = rho[i] * u[i];
-		W[2 * nx + i] = e[i];
+    // Inlet flow properties
+    Mach[0]=Min;
+    T[0] = isenT(Ttin, Mach[0]);
+    p[0] = isenP(ptin, Mach[0]);
+    rho[0] = p[0] / (R * T[0]);
+    c[0] = sqrt(gam * p[0] / rho[0]);
+    u[0] = Mach[0] * c[0];
+    e[0] = rho[0] * (Cv * T[0] + 0.5 * pow(u[0], 2));
 
-		F[0 * nx + i] = rho[i] * u[i];
-		F[1 * nx + i] = rho[i] * u[i] * u[i] + p[i];
-		F[2 * nx + i] = ( e[i] + p[i] ) * u[i];
-	}
+    // Flow Properties Initialization
+    for(int i = 1; i < nx; i++)
+    {
+        Mach[i] = Min;
+        Mach[i] = Min;
+        p[i] = pexit;
+        T[i] = T[0];
+        rho[i] = p[i] / (R * T[i]);
+        c[i] = sqrt(gam * p[i] / rho[i]);
+        u[i] = c[i] * Mach[i];
+        e[i] = rho[i] * (Cv * T[i] + 0.5 * pow(u[i], 2));
+    }
 
-	for(int i = 1; i < nx - 1; i++)
-	{
-		Q[1 * nx + i] = p[i] * (S[i + 1] - S[i]);
-	}
+    // State and Flux Vectors Initialization
+    for(int i = 0; i < nx; i++)
+    {
+        W[0 * nx + i] = rho[i];
+        W[1 * nx + i] = rho[i] * u[i];
+        W[2 * nx + i] = e[i];
 
-	while(normR > conv && iterations < maxIt)
-	{
-		iterations++;
+        F[0 * nx + i] = rho[i] * u[i];
+        F[1 * nx + i] = rho[i] * u[i] * u[i] + p[i];
+        F[2 * nx + i] = ( e[i] + p[i] ) * u[i];
+    }
 
-		if(iterations%printIt == 0) 
-		{
-			if(printConv == 1)
-			{
-				std::cout<<"Iteration "<<iterations
-				         <<"   NormR "<<std::setprecision(15)<<normR<<std::endl;
-			}
-			itV[iterations / printIt - 1] = iterations;
-			normV[iterations / printIt - 1] = normR;
-		}
+    for(int i = 1; i < nx - 1; i++)
+    {
+        Q[1 * nx + i] = p[i] * (S[i + 1] - S[i]);
+    }
 
-		for(int i = 0; i < nx; i++)
-			dt[i] = (CFL * dx[i]) / fabs(u[i] + c[i]);
+    while(normR > conv && iterations < maxIt)
+    {
+        iterations++;
+
+        if(iterations%printIt == 0) 
+        {
+            if(printConv == 1)
+            {
+                std::cout<<"Iteration "<<iterations
+                         <<"   NormR "<<std::setprecision(15)<<normR<<std::endl;
+            }
+            itV[iterations / printIt - 1] = iterations;
+            normV[iterations / printIt - 1] = normR;
+        }
+
+        for(int i = 0; i < nx; i++)
+            dt[i] = (CFL * dx[i]) / fabs(u[i] + c[i]);
 
         if(FluxScheme == 0) // SW
-    		Flux_StegerWarmingV(Flux, W, u, c, rho);
+            Flux_StegerWarmingV(Flux, W, u, c, rho);
         else if(FluxScheme == 1) // Scalar
-    		Flux_Scalar(Flux, W, F, u, c);
-		
-		if(StepScheme == 0)
-		{
-			EulerExplicitStep(S, V, dt, Flux, Q, Resi, W);
-		}
-		else if(StepScheme == 1)
-		{
-			rk4(dx, S, dt, W, Q, Resi, Flux);
-		}
+            Flux_Scalar(Flux, W, F, u, c);
+        
+        if(StepScheme == 0)
+        {
+            EulerExplicitStep(S, V, dt, Flux, Q, Resi, W);
+        }
+        else if(StepScheme == 1)
+        {
+            rk4(dx, S, dt, W, Q, Resi, Flux);
+        }
 
 
-		// Inlet Boundary Condition
-		if(Mach[0] < 1)
-		{
-			dpdu = ptin * (gam / (gam - 1))
-				 * pow(1 - ((gam - 1) / (gam + 1)) * u[0] * u[0] / a2,
+        // Inlet Boundary Condition
+        if(Mach[0] < 1)
+        {
+            dpdu = ptin * (gam / (gam - 1))
+                 * pow(1 - ((gam - 1) / (gam + 1)) * u[0] * u[0] / a2,
                        1 / (gam - 1))
-				 * ( - 2 * ((gam - 1) / (gam + 1)) * u[0] / a2);
+                 * ( - 2 * ((gam - 1) / (gam + 1)) * u[0] / a2);
                  
-			eigenvalues[0] = ((u[1] + u[0] - c[1] - c[0]) / 2) * (dt[0] / dx[0]);
+            eigenvalues[0] = ((u[1] + u[0] - c[1] - c[0]) / 2) * (dt[0] / dx[0]);
             
             dpdx = p[1] - p[0];
             dudx = u[1] - u[0];
-			du = -eigenvalues[0] * (dpdx - rho[0] * c[0] * dudx)
-				 / (dpdu - rho[0] * c[0]);
+            du = -eigenvalues[0] * (dpdx - rho[0] * c[0] * dudx)
+                 / (dpdu - rho[0] * c[0]);
 
-			u[0] = u[0] + du;
-			T[0] = Ttin * (1 - ((gam - 1) / (gam + 1)) * u[0] * u[0] / a2);
-			p[0] = ptin * pow(T[0] / Ttin, gam / (gam - 1));
-			rho[0] = p[0] / (R * T[0]);
-			e[0] = rho[0] * (Cv * T[0] + 0.5 * u[0] * u[0]);
-			c[0] = sqrt(gam * p[0] / rho[0]);
-			Mach[0] = u[0] / c[0];
-		}
+            u[0] = u[0] + du;
+            T[0] = Ttin * (1 - ((gam - 1) / (gam + 1)) * u[0] * u[0] / a2);
+            p[0] = ptin * pow(T[0] / Ttin, gam / (gam - 1));
+            rho[0] = p[0] / (R * T[0]);
+            e[0] = rho[0] * (Cv * T[0] + 0.5 * u[0] * u[0]);
+            c[0] = sqrt(gam * p[0] / rho[0]);
+            Mach[0] = u[0] / c[0];
+        }
 
-		// Exit boundary condition
+        // Exit boundary condition
         avgu = (u[nx - 1] + u[nx - 2]) / 2;
         avgc = (c[nx - 1] + c[nx - 2]) / 2;
         dtdx = dt[nx - 1] / dx[nx - 1];
-		eigenvalues[0] = avgu * dtdx;
-		eigenvalues[1] = (avgu + avgc) * dtdx;
-		eigenvalues[2] = (avgu - avgc) * dtdx;
+        eigenvalues[0] = avgu * dtdx;
+        eigenvalues[1] = (avgu + avgc) * dtdx;
+        eigenvalues[2] = (avgu - avgc) * dtdx;
 
         dpdx = p[nx - 1] - p[nx - 2];
         dudx = u[nx - 1] - u[nx - 2];
 
-		charRel[0] = -eigenvalues[0] * ( rho[nx - 1] - rho[nx - 2] - dpdx / pow(c[nx - 1], 2) );
-		charRel[1] = -eigenvalues[1] * ( dpdx + rho[nx - 1] * c[nx - 1] * dudx );
-		charRel[2] = -eigenvalues[2] * ( dpdx - rho[nx - 1] * c[nx - 1] * dudx );
+        charRel[0] = -eigenvalues[0] * ( rho[nx - 1] - rho[nx - 2] - dpdx / pow(c[nx - 1], 2) );
+        charRel[1] = -eigenvalues[1] * ( dpdx + rho[nx - 1] * c[nx - 1] * dudx );
+        charRel[2] = -eigenvalues[2] * ( dpdx - rho[nx - 1] * c[nx - 1] * dudx );
 
-		MachBound = avgu / avgc;
-		//MachBound = u[nx - 1] / c[nx - 1];
- 		if(MachBound > 1)
+        MachBound = avgu / avgc;
+        //MachBound = u[nx - 1] / c[nx - 1];
+        if(MachBound > 1)
         {
-		    dp = 0.5 * (charRel[1] + charRel[2]);
+            dp = 0.5 * (charRel[1] + charRel[2]);
         }
-		else
+        else
         {
-		    dp = 0;
+            dp = 0;
         }
-	
-		drho = charRel[0] + dp / (pow(c[nx - 1], 2));
-		du = (charRel[1] - dp) / (rho[nx - 1] * c[nx - 1]);
-	
-		u[nx - 1] = u[nx - 1] + du;
-		rho[nx - 1] = rho[nx - 1] + drho;
-		p[nx - 1] = p[nx - 1] + dp;
-		T[nx - 1] = p[nx - 1] / (rho[nx - 1] * R);
-		e[nx - 1] = rho[nx - 1] * (Cv * T[nx - 1] + 0.5 * pow(u[nx - 1], 2));
-		c[nx - 1] = sqrt((gam * p[nx - 1]) / rho[nx - 1]);
-		Mach[nx - 1] = u[nx - 1] / c[nx - 1];
+    
+        drho = charRel[0] + dp / (pow(c[nx - 1], 2));
+        du = (charRel[1] - dp) / (rho[nx - 1] * c[nx - 1]);
+    
+        u[nx - 1] = u[nx - 1] + du;
+        rho[nx - 1] = rho[nx - 1] + drho;
+        p[nx - 1] = p[nx - 1] + dp;
+        T[nx - 1] = p[nx - 1] / (rho[nx - 1] * R);
+        e[nx - 1] = rho[nx - 1] * (Cv * T[nx - 1] + 0.5 * pow(u[nx - 1], 2));
+        c[nx - 1] = sqrt((gam * p[nx - 1]) / rho[nx - 1]);
+        Mach[nx - 1] = u[nx - 1] / c[nx - 1];
 
-		// Update flow properties
-		for(int i = 1; i < nx - 1; i++)
-		{
-		    rho[i] = W[0 * nx + i];		// rho
-		    u[i] = W[1 * nx + i] / rho[i];	// U
-		    p[i] = (gam - 1) * (W[2 * nx + i] - rho[i] * pow(u[i], 2) / 2);  // Pressure
-		    T[i] = p[i] / (rho[i] * R);	// Temperature
-		    e[i] = W[2 * nx + i];		// Energy
-		    c[i] = sqrt((gam * p[i]) / rho[i]);// Speed of sound
-		    Mach[i] = u[i] / c[i];		// Mach number
-		}
+        // Update flow properties
+        for(int i = 1; i < nx - 1; i++)
+        {
+            rho[i] = W[0 * nx + i];     // rho
+            u[i] = W[1 * nx + i] / rho[i];  // U
+            p[i] = (gam - 1) * (W[2 * nx + i] - rho[i] * pow(u[i], 2) / 2);  // Pressure
+            T[i] = p[i] / (rho[i] * R); // Temperature
+            e[i] = W[2 * nx + i];       // Energy
+            c[i] = sqrt((gam * p[i]) / rho[i]);// Speed of sound
+            Mach[i] = u[i] / c[i];      // Mach number
+        }
 
-		// Update vectors 
-		for(int i = 0; i < nx; i++)
-		{
-		    W[0 * nx + i] = rho[i];
-		    W[1 * nx + i] = rho[i] * u[i];
-		    W[2 * nx + i] = e[i];
-		    F[0 * nx + i] = rho[i] * u[i];
-		    F[1 * nx + i] = rho[i] * u[i] * u[i] + p[i];
-		    F[2 * nx + i] = (e[i] + p[i]) * u[i];
-		    Q[0 * nx + i] = 0;
-		    Q[1 * nx + i] = p[i] * (S[i + 1] - S[i]);
-		    Q[2 * nx + i] = 0;
-		}
+        // Update vectors 
+        for(int i = 0; i < nx; i++)
+        {
+            W[0 * nx + i] = rho[i];
+            W[1 * nx + i] = rho[i] * u[i];
+            W[2 * nx + i] = e[i];
+            F[0 * nx + i] = rho[i] * u[i];
+            F[1 * nx + i] = rho[i] * u[i] * u[i] + p[i];
+            F[2 * nx + i] = (e[i] + p[i]) * u[i];
+            Q[0 * nx + i] = 0;
+            Q[1 * nx + i] = p[i] * (S[i + 1] - S[i]);
+            Q[2 * nx + i] = 0;
+        }
         
-		// Calculating the norm of the density residual
-		normR = 0;
-		for(int i = 0; i < nx; i++)
-		    normR = normR + Resi[0 * nx + i] * Resi[0 * nx + i];
-		normR = sqrt(normR);
-	}
+        // Calculating the norm of the density residual
+        normR = 0;
+        for(int i = 0; i < nx; i++)
+            normR = normR + Resi[0 * nx + i] * Resi[0 * nx + i];
+        normR = sqrt(normR);
+    }
 
-	if(printW == 1)
-	{
-		for(int k = 0; k < 3; k++)
-		{
-			std::cout<<"W"<<k + 1<<std::endl;
-			for(int i = 0; i < nx; i++)
-				std::cout<<W[k * nx + i]<<std::endl;
-		}
-	}
-	std::cout<<"Flow iterations = "<<iterations<<"   Density Residual = "<<normR<<std::endl;
-	
+    if(printW == 1)
+    {
+        for(int k = 0; k < 3; k++)
+        {
+            std::cout<<"W"<<k + 1<<std::endl;
+            for(int i = 0; i < nx; i++)
+                std::cout<<W[k * nx + i]<<std::endl;
+        }
+    }
+    std::cout<<"Flow iterations = "<<iterations<<"   Density Residual = "<<normR<<std::endl;
+    
 
-	FILE  * Results;
-	Results = fopen("Results.dat", "w");
-	fprintf(Results, "%d\n", nx);
-	for(int i = 0; i < nx; i++)
-		fprintf(Results, "%.15f\n", x[i]);
-	for(int i = 0; i < nx; i++)
-		fprintf(Results, "%.15f\n", p[i] / ptin);
-	for(int i = 0; i < nx; i++)
-		fprintf(Results, "%.15f\n", rho[i]);
-	for(int i = 0; i < nx; i++)
-		fprintf(Results, "%.15f\n", Mach[i]);
-	for(int i = 0; i < nx; i++)
-		fprintf(Results, "%.15f\n", x[i] - dx[i] / 2);
-	fprintf(Results, "%f\n", x.back() + dx.back() / 2);
+    FILE  * Results;
+    Results = fopen("Results.dat", "w");
+    fprintf(Results, "%d\n", nx);
+    for(int i = 0; i < nx; i++)
+        fprintf(Results, "%.15f\n", x[i]);
+    for(int i = 0; i < nx; i++)
+        fprintf(Results, "%.15f\n", p[i] / ptin);
+    for(int i = 0; i < nx; i++)
+        fprintf(Results, "%.15f\n", rho[i]);
+    for(int i = 0; i < nx; i++)
+        fprintf(Results, "%.15f\n", Mach[i]);
+    for(int i = 0; i < nx; i++)
+        fprintf(Results, "%.15f\n", x[i] - dx[i] / 2);
+    fprintf(Results, "%f\n", x.back() + dx.back() / 2);
 
-	iterlength = itV.size();
-	for(int i = 0; i < nx + 1; i++)
-		fprintf(Results, "%.15f\n", S[i]);
-	for(int i = 0; i < iterlength; i++)
-		fprintf(Results, "%.15d\n", itV[i]);
-	for(int i = 0; i < iterlength; i++)
-		fprintf(Results, "%.15f\n", normV[i]);
-
-
-
-	fclose(Results);
-
-	// Create Target Pressure
-	if(createTarget == 1) ioTargetPressure(1, p);
+    iterlength = itV.size();
+    for(int i = 0; i < nx + 1; i++)
+        fprintf(Results, "%.15f\n", S[i]);
+    for(int i = 0; i < iterlength; i++)
+        fprintf(Results, "%.15d\n", itV[i]);
+    for(int i = 0; i < iterlength; i++)
+        fprintf(Results, "%.15f\n", normV[i]);
 
 
-	if(fitnessFun == 0)
-		return TotalPressureLoss(W);
-	else if(fitnessFun == 1)
-	{
-		std::vector <double> ptarget(nx, 0);
-		ioTargetPressure(-1, ptarget);
-		return inverseFitness(p, ptarget, dx);
-	}
+
+    fclose(Results);
+
+    // Create Target Pressure
+    if(createTarget == 1) ioTargetPressure(1, p);
 
 
-	return  - 9999.99;
+    if(fitnessFun == 0)
+        return TotalPressureLoss(W);
+    else if(fitnessFun == 1)
+    {
+        std::vector <double> ptarget(nx, 0);
+        ioTargetPressure(-1, ptarget);
+        return inverseFitness(p, ptarget, dx);
+    }
+
+
+    return  - 9999.99;
 
 }
 
 double isenP(double pt, double M)
 {
-	return pt * pow((1 + (gam - 1) / 2 * pow(M, 2)), ( - gam / (gam - 1)));
+    return pt * pow((1 + (gam - 1) / 2 * pow(M, 2)), ( - gam / (gam - 1)));
 }
 
 double isenT(double Tt, double M)
 {
-	return Tt * pow((1 + (gam - 1) / 2 * pow(M, 2)), - 1);
+    return Tt * pow((1 + (gam - 1) / 2 * pow(M, 2)), - 1);
 }
 
 double TotalPressureLoss(std::vector <double> W)
 {
-	double rhoout = W[0 * nx + (nx - 1)];
-	double uout = W[1 * nx + (nx - 1)] / rhoout;
-	double pout = (gam - 1) * (W[2 * nx + (nx - 1)] - rhoout * pow(uout, 2) / 2);
-	//double Tout = pout/(rhoout * R);
+    double rhoout = W[0 * nx + (nx - 1)];
+    double uout = W[1 * nx + (nx - 1)] / rhoout;
+    double pout = (gam - 1) * (W[2 * nx + (nx - 1)] - rhoout * pow(uout, 2) / 2);
+    //double Tout = pout/(rhoout * R);
 
-	double ptout_normalized;
+    double ptout_normalized;
 
-	double ToverTt = 1 - pow(uout, 2) / a2 * (gam - 1) / (gam + 1);
+    double ToverTt = 1 - pow(uout, 2) / a2 * (gam - 1) / (gam + 1);
 
-	double poverpt = pow(ToverTt, (gam / (gam - 1)));
+    double poverpt = pow(ToverTt, (gam / (gam - 1)));
 
-	ptout_normalized = 1 - (pout / poverpt) / ptin;
+    ptout_normalized = 1 - (pout / poverpt) / ptin;
 
-	return ptout_normalized;
+    return ptout_normalized;
 }
 
 // Define Volume
 std::vector <double> calcVolume(std::vector <double> S, std::vector <double> dx)
 {
-	std::vector <double> V;
-	int ndx = dx.size();
-	for(int i = 0; i < ndx; i++)
-		V.push_back((S[i] + S[i + 1]) / 2 * dx[i]);
+    std::vector <double> V;
+    int ndx = dx.size();
+    for(int i = 0; i < ndx; i++)
+        V.push_back((S[i] + S[i + 1]) / 2 * dx[i]);
 
-	return V;
+    return V;
 }
 
 // Input/Output Target Pressure Distribution
 void ioTargetPressure(int io, std::vector <double> &p)
 {
 
-	FILE  * TargetP;
-	// Output
-	if(io > 0)
-	{
-		TargetP = fopen("targetP.dat", "w");
-		fprintf(TargetP, "%d\n", nx);
-//		for(int i = 0; i < nx; i++)
-//			fprintf(TargetP, "%.15f\n", x[i]);
-		for(int i = 0; i < nx; i++)
-			fprintf(TargetP, "%.15f\n", p[i] / ptin);
-	}
-	// Input
-	else
-	{
-		int nxT;
+    FILE  * TargetP;
+    // Output
+    if(io > 0)
+    {
+        TargetP = fopen("targetP.dat", "w");
+        fprintf(TargetP, "%d\n", nx);
+//      for(int i = 0; i < nx; i++)
+//          fprintf(TargetP, "%.15f\n", x[i]);
+        for(int i = 0; i < nx; i++)
+            fprintf(TargetP, "%.15f\n", p[i] / ptin);
+    }
+    // Input
+    else
+    {
+        int nxT;
 
-		TargetP = fopen("targetP.dat", "r");
-		rewind(TargetP);
-		fscanf(TargetP, "%d", &nxT);
+        TargetP = fopen("targetP.dat", "r");
+        rewind(TargetP);
+        fscanf(TargetP, "%d", &nxT);
         if(nxT!=nx) std::cout<< "nx and nxT are different for targetP";
-		for(int iT = 0; iT < nxT; iT++)
-		{
-			fscanf(TargetP, "%lf", &p[iT]);
-		}
-	}	
+        for(int iT = 0; iT < nxT; iT++)
+        {
+            fscanf(TargetP, "%lf", &p[iT]);
+        }
+    }   
 
 
-	fclose(TargetP);
+    fclose(TargetP);
 
 }
 
 // Return Inverse Design Fitness
 
 double inverseFitness(std::vector <double> pcurrent, std::vector <double> ptarget,
-		std::vector <double> dx)
+        std::vector <double> dx)
 {
-	double fit = 0;
-	for(int i = 0; i < nx; i++)
-	{
-		fit += pow(pcurrent[i] / ptin - ptarget[i], 2) * dx[i];
-	}
-	std::cout<<"InverseFitness =  "<<fit / 2<<std::endl;
-	return fit / 2;
+    double fit = 0;
+    for(int i = 0; i < nx; i++)
+    {
+        fit += pow(pcurrent[i] / ptin - ptarget[i], 2) * dx[i];
+    }
+    std::cout<<"InverseFitness =  "<<fit / 2<<std::endl;
+    return fit / 2;
 }
